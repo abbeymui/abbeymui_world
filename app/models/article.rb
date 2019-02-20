f81f76268944c49bcac2cea2dd03b55bf41c3406
Conlon Novak <conlonn@andrew.cmu.edu>
2019-02-20 05:31:32 +0000

    Spring Cleaning (misc. merge conflicts that weren't resolved are now removed)

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,5 @@
 class Article < ApplicationRecord
-<<<<<<< HEAD
-    has_one :category
-=======
     belongs_to :category
->>>>>>> recovery
 
     validates_presence_of :title
     validates_presence_of :content
